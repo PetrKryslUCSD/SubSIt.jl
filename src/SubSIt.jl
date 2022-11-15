6c1ec870b6a30ba45cfdc1c7d51b9026dd614eda
Petr Krysl <pkrysl@ucsd.edu>
2022-11-15 09:42:00 -0800

    implement the Bathe Table 11.3 algorithm

--- src/SubSIt.jl
+++ src/SubSIt.jl
@@ -37,7 +37,7 @@ Keyword arguments
     successive  estimates of the eigenvalues (or not normalized if the 
     eigenvalues converge to zero).
 """
-function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), tol = 1.0e-3, maxiter = 300, verbose=false, which=:SM, check=0) 
+function ssit1(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), tol = 1.0e-3, maxiter = 300, verbose=false, which=:SM, check=0) 
     @assert which == :SM
     @assert nev >= 1
     if size(v0) == (0, 0)
@@ -89,10 +89,8 @@ function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), t
         ix = sortperm(real.(decomp.values))
         evalues = real.(decomp.values[ix])
         evectors = decomp.vectors[:, ix]
-        # v .= v * real.(evectors)
         mul!(Mv, v, real.(evectors))
         v, Mv = Mv, v
-        # @show leps =  sqrt(norm(lamb[1:nev] - real.(evalues)[1:nev]) / norm(lamb[1:nev]))
         plamb .= evalues
         for j in 1:nvecs
             if abs(lamb[j]) <= tol  # zero eigenvalues
@@ -107,66 +105,11 @@ function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), t
         if nconv >= nev # converged on all requested eigenvalues
             break
         end
-        plamb, lamb = lamb, plamb # swap the eigenvalue arrays
         niter = niter + 1
     end
     __mass_normalize_M!(v, M)
     return lamb[1:nev], v[:, 1:nev], nconv, niter, lamberr
 end
-
-# function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), tol = 1.0e-3, maxiter = 300, verbose=false, which=:SM, check=0) 
-#     @assert which == :SM
-#     @assert nev >= 1
-#     if size(v0) == (0, 0)
-#         v0 = [i==j ? one(eltype(K)) : zero(eltype(K)) for i=1:size(K,1), j=1:ncv]
-#     end
-#     v = deepcopy(v0)
-#     @assert nev <= size(v0, 2)
-#     nvecs = size(v, 2)  # How many eigenvalues are iterated?
-#     plamb = zeros(nvecs)  # previous eigenvalue
-#     lamb = zeros(nvecs)
-#     lamberr = zeros(nvecs)
-#     converged = falses(nvecs)  # not yet
-#     niter = 0
-#     nconv = 0
-#     factor = cholesky(K)
-#     Kv = zeros(size(K, 1), nvecs)
-#     Mv = zeros(size(M, 1), nvecs)
-    
-#     for i = 1:maxiter
-#         mul!(Mv, M, v)
-#         v .= factor \ Mv
-#         mul!(Mv, M, v)
-#         __mass_normalize!(v, Mv)
-#         mul!(Kv, K, v)
-#         mul!(Mv, M, v)
-#         decomp = eigen(transpose(v)*Kv, transpose(v)*Mv)
-#         ix = sortperm(real.(decomp.values))
-#         evalues = decomp.values[ix]
-#         evectors = decomp.vectors[:, ix]
-#         # v .= v * real.(evectors)
-#         mul!(Mv, v, real.(evectors))
-#         v, Mv = Mv, v
-#         lamb .= real.(evalues)
-#         for j = 1:nvecs
-#             if abs(lamb[j]) <= tol # zero eigenvalues
-#                 lamberr[j] = abs(lamb[j])
-#             else # non zero eigenvalues
-#                 lamberr[j] = abs(lamb[j] - plamb[j])/abs(lamb[j])
-#             end
-#             converged[j] = lamberr[j] <= tol
-#         end
-#         nconv = length(findall(converged[1:nev]))
-#         verbose && println("nconv = $(nconv)")
-#         if nconv >= nev # converged on all requested eigenvalues
-#             break
-#         end
-#         plamb, lamb = lamb, plamb # swap the eigenvalue arrays
-#         niter = niter + 1
-#     end
-#     __mass_normalize_M!(v, M)
-#     return lamb[1:nev], v[:, 1:nev], nconv, niter, lamberr
-# end
 
 function __mass_normalize!(v, Mv)
     for k in axes(v, 2)
@@ -180,6 +123,66 @@ function __mass_normalize_M!(v, M)
         v[:, k] ./= @views sqrt(v[:, k]' * M * v[:, k])
     end
     v
+end
+
+function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), tol = 1.0e-3, maxiter = 300, verbose=false, which=:SM, check=0) 
+    @assert which == :SM
+    @assert nev >= 1
+    ncv = max(ncv, size(v0, 2))
+    @assert nev < ncv
+    nvecs = ncv
+    if size(v0) == (0, 0)
+        v0 = fill(zero(eltype(K)), size(K,1), nvecs)
+        for j in axes(M, 1)
+            v0[j, 1] = M[j, j]
+        end
+        dMK = diag(M) ./ diag(K)
+        ix = sortperm(dMK)
+        k = 1
+        for j in 2:nvecs-1
+            v0[ix[k], j] = 1.0
+        end
+        v0[:, end] = rand(size(K,1))
+    end
+    X = v0
+    Y = deepcopy(X)
+    Kr = fill(zero(eltype(K)), nvecs, nvecs)
+    Mr = fill(zero(eltype(K)), nvecs, nvecs)
+    plamb = fill(zero(eltype(K)), nvecs) .+ Inf
+    lamb = fill(zero(eltype(K)), nvecs)
+    lamberr = fill(zero(eltype(K)), nvecs)
+    converged = falses(nev)  # not yet
+    niter = 0
+    nconv = 0
+
+    factor = cholesky(K)
+    mul!(Y, M, X)
+    for i = 1:maxiter
+        X .= factor \ Y
+        mul!(Kr, transpose(X), Y)
+        mul!(Y, M, X)
+        mul!(Mr, transpose(X), Y)
+        decomp = eigen(Kr, Mr)
+        ix = sortperm(real.(decomp.values))
+        evalues = real.(decomp.values[ix])
+        evectors = decomp.vectors[:, ix]
+        mul!(X, Y, real.(evectors))
+        __mass_normalize_M!(X, M)
+        X, Y = Y, X
+        lamb .= evalues
+        for j in 1:nvecs
+            lamberr[j] = abs(lamb[j] - plamb[j]) / abs(plamb[j])
+        end
+        converged .= (lamberr .<= tol)[1:nev]
+        nconv = length(findall(converged))
+        verbose && println("nconv = $(nconv)")
+        if nconv >= nev # converged on all requested eigenvalues
+            break
+        end
+        lamb, plamb = plamb, lamb
+        niter = niter + 1
+    end
+    return lamb[1:nev], Y[:, 1:nev], nconv, niter, lamberr
 end
 
 end
