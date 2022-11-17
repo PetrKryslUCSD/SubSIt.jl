3d7aa4dbca6f26436275a2316b28afa7d6091051
Petr Krysl <pkrysl@ucsd.edu>
2022-11-16 18:25:18 -0800

    update tests

---
+++ test/test_barrel.jl
@@ -0,0 +1,144 @@
+
+# """
+# Vibration analysis of free-floating thin plate
+
+# Free-vibration problem is solved for a homogeneous free-floating
+# (unsupported) square plate. This is the NAFEMS Benchmark, Test No. FV12.
+
+# The plate is discretized with shell elements. Because no displacements are
+# prevented, the structure has six rigid body modes (six zero vibration
+# frequencies).
+
+# The nonzero benchmark frequencies are (in hertz): 
+
+# 1.622, 2.360, 2.922, 4.190, 4.190,  7.356, 7.356, 7.668.
+# """
+module barrel_w_stiffeners_examples
+
+using Test
+using LinearAlgebra
+using Arpack
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FEMMShellT3FFModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
+using DataDrop
+using DataDrop: with_extension
+using SubSIt: ssit
+# using KrylovKit
+using FinEtools.MeshExportModule.VTKWrite: vtkwrite
+
+function _execute_model(formul, input, visualize = true)
+    E = 200e3*phun("MPa")
+    nu = 0.3;
+    rho= 7850*phun("KG/M^3");
+    thickness = 2.0*phun("mm");
+    OmegaShift = (100*2*pi)^2
+    
+    # Report
+    @info "Mesh: $input"
+
+    if !isfile(joinpath(dirname(@__FILE__()), input))
+        success(run(`unzip -qq -d $(dirname(@__FILE__())) $(joinpath(dirname(@__FILE__()), "barrel_w_stiffeners-s3-mesh.zip"))`; wait = false))
+    end
+    output = FinEtools.MeshImportModule.import_H5MESH(joinpath(dirname(@__FILE__()), input))
+    fens, fes  = output["fens"], output["fesets"][1]
+    fens.xyz .*= phun("mm");
+
+    # output = import_ABAQUS(joinpath(dirname(@__FILE__()), input))
+    # fens = output["fens"]
+    # fes = output["fesets"][1]
+
+    # connected = findunconnnodes(fens, fes);
+    # fens, new_numbering = compactnodes(fens, connected);
+    # fes = renumberconn!(fes, new_numbering);
+
+    # fens, fes = mergenodes(fens, fes, thickness/10)
+    
+    # FinEtools.MeshExportModule.H5MESH.write_H5MESH(with_extension(input, "h5mesh"), fens, fes)
+    
+    @info "Number of nodes and elements: $(count(fens)), $(count(fes))"
+
+    mater = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
+    
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    associate = formul.associategeometry!
+    stiffness = formul.stiffness
+    mass = formul.mass
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # l1 = collect(1:count(fens))
+    # for i in [6]
+    #     setebc!(dchi, l1, true, i)
+    # end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    tim = @elapsed begin
+        associategeometry!(femm, geom0)
+    end
+    @info "Associate Geometry time $tim [sec]"
+    tim = @elapsed begin
+        K = stiffness(femm, geom0, u0, Rfield0, dchi);
+    end
+    @info "Stiffness assembly time $tim [sec]"
+    tim = @elapsed begin
+        M = mass(femm, geom0, dchi);
+    end
+    @info "Mass assembly time $tim [sec]"
+
+    # DataDrop.store_matrix("barrel_w_stiffeners.h5", "/K", K)
+    # DataDrop.store_matrix("barrel_w_stiffeners.h5", "/M", M)
+
+    # Solve
+    neigvs = 40
+
+    tim = @elapsed begin
+        evals, evecs, convinfo = ssit(Symmetric(K+OmegaShift*M), Symmetric(M); nev=neigvs, verbose=true)
+    end
+    @show convinfo
+    evals[:] = evals .- OmegaShift;
+    fs = real(sqrt.(complex(evals)))/(2*pi)
+    @info "Frequencies: $(round.(fs[7:15], digits=4))"
+    reffs = fs
+
+    @info "SubSIt: Time $tim [sec]"
+
+    tim = @elapsed begin
+        evals, evecs, convinfo = eigs(Symmetric(K+OmegaShift*M), Symmetric(M); nev=neigvs, which=:SM, explicittransform=:none)
+    end
+    @show convinfo
+    evals[:] = evals .- OmegaShift;
+    fs = real(sqrt.(complex(evals)))/(2*pi)
+    @info "Frequencies: $(round.(fs[7:15], digits=4))"
+    @info "Arpack: Time $tim [sec]"
+
+    @test norm(reffs - fs) / norm(reffs) < 1.0e-3
+
+    return true
+end
+
+
+function test_convergence()
+    input = "barrel_w_stiffeners-s3.h5mesh"
+    formul = FEMMShellT3FFModule
+    @info "Barrel With Stiffeners, free vibration, formulation=$(formul)"
+    @time _execute_model(formul, input, true)
+    return true
+end
+
+test_convergence()
+
+end # module
+nothing
--- test/test_cube.jl
+++ test/test_cube.jl
@@ -1,27 +1,95 @@
-module mcub001
+# module mcub001
+# using Test
+# using LinearAlgebra
+# using Arpack
+# using SubSIt: ssit
+# using DataDrop
+# neigvs = 20                   # how many eigenvalues
+# OmegaShift = (0.01*2*pi)^2;
+
+# function test_ssit(f, reffs, tol = 0.001)
+#     K = DataDrop.retrieve_matrix(f, "/K")
+#     M = DataDrop.retrieve_matrix(f, "/M")
+#     @info "Matrix size = $(size(K))"
+#     d,v,nconv = ssit(Symmetric(K+OmegaShift*M), Symmetric(M); nev=neigvs, which=:SM, check=1)
+#     d = d .- OmegaShift;
+#     fs = real(sqrt.(complex(d)))/(2*pi)
+#     @test norm(fs - reffs) < tol
+#     true
+# end
+# for d in (
+#     ("unit_cube_8.h5", [0.00000e+00, 0.00000e+00, 3.16833e-08, 1.09714e-07, 1.12730e-07, 1.87727e-07, 2.51509e-01, 2.56099e-01, 3.37083e-01, 3.39137e-01, 3.42637e-01, 3.48691e-01, 3.49059e-01, 3.50360e-01, 3.77199e-01, 3.96925e-01, 3.97760e-01, 4.27904e-01, 4.29469e-01, 4.30729e-01]),
+#     ("unit_cube_16.h5", [0.00000e+00, 0.00000e+00, 1.39971e-07, 1.98557e-07, 2.46956e-07, 3.04491e-07, 2.59393e-01, 2.60642e-01, 3.51863e-01, 3.52430e-01, 3.53351e-01, 3.57428e-01, 3.57552e-01, 3.57918e-01, 3.99576e-01, 4.05492e-01, 4.05636e-01, 4.52988e-01, 4.53485e-01, 4.54343e-01])
+#     )
+#     test_ssit(d...)
+# end
+
+# end
+
+module unit_cube_tet_examples
+
 using Test
+using FinEtools
+using FinEtools.MeshExportModule
+using FinEtoolsDeforLinear
+using FinEtoolsDeforLinear.AlgoDeforLinearModule: ssit
 using LinearAlgebra
 using Arpack
-using SubSIt: ssit
 using DataDrop
+
+E = 1*phun("PA");
+nu = 0.499;
+rho = 1*phun("KG/M^3");
+a = 1*phun("M"); b = a; h =  a;
+n1 = 16;# How many element edges per side?
+na =  n1; nb =  n1; nh  = n1;
 neigvs = 20                   # how many eigenvalues
 OmegaShift = (0.01*2*pi)^2;
 
-function test_ssit(f, reffs, tol = 0.001)
-    K = DataDrop.retrieve_matrix(f, "/K")
-    M = DataDrop.retrieve_matrix(f, "/M")
-    @info "Matrix size = $(size(K))"
-    d,v,nconv = ssit(Symmetric(K+OmegaShift*M), Symmetric(M); nev=neigvs, which=:SM, check=1)
+function unit_cube_esnice_ssit(N, reffs, tol=0.001)
+    na,nb,nh = N, N, N
+    # println("""
+    # Vibration modes of unit cube  of almost incompressible material.
+
+    # Reference: Puso MA, Solberg J (2006) A stabilized nodally integrated
+    # tetrahedral. International Journal for Numerical Methods in
+    # Engineering 67: 841-867.
+    # """)
+
+    MR = DeforModelRed3D
+    fens,fes  = T4block(a,b,h, na,nb,nh)
+
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz,1),3)) # displacement field
+
+    numberdofs!(u)
+
+    material = MatDeforElastIso(MR, rho, E, nu, 0.0)
+
+    femm = FEMMDeforLinearESNICET4(MR, IntegDomain(fes, NodalSimplexRule(3)), material)
+    associategeometry!(femm,  geom)
+    K  = stiffness(femm, geom, u)
+    M = mass(femm, geom, u)
+    @time d,v,nconv = eigs(K+OmegaShift*M, M; nev=neigvs)
+    @time d,v,nconv = ssit(K+OmegaShift*M, M; nev=neigvs)
     d = d .- OmegaShift;
     fs = real(sqrt.(complex(d)))/(2*pi)
+    println("Eigenvalues: $fs [Hz]")
     @test norm(fs - reffs) < tol
-    true
-end
-for d in (
-    ("unit_cube_8.h5", [0.00000e+00, 0.00000e+00, 3.16833e-08, 1.09714e-07, 1.12730e-07, 1.87727e-07, 2.51509e-01, 2.56099e-01, 3.37083e-01, 3.39137e-01, 3.42637e-01, 3.48691e-01, 3.49059e-01, 3.50360e-01, 3.77199e-01, 3.96925e-01, 3.97760e-01, 4.27904e-01, 4.29469e-01, 4.30729e-01]),
-    ("unit_cube_16.h5", [0.00000e+00, 0.00000e+00, 1.39971e-07, 1.98557e-07, 2.46956e-07, 3.04491e-07, 2.59393e-01, 2.60642e-01, 3.51863e-01, 3.52430e-01, 3.53351e-01, 3.57428e-01, 3.57552e-01, 3.57918e-01, 3.99576e-01, 4.05492e-01, 4.05636e-01, 4.52988e-01, 4.53485e-01, 4.54343e-01])
-    )
-    test_ssit(d...)
-end
 
+    # mode = 17
+    # scattersysvec!(u, v[:,mode])
+    # File =  "unit_cube_esnice.vtk"
+    # vtkexportmesh(File, fens, fes; vectors=[("mode$mode", u.values)])
+    # @async run(`"paraview.exe" $File`)
+    true
+end # unit_cube_esnice
+
+for d in (
+    (8, [0.00000e+00, 0.00000e+00, 3.16833e-08, 1.09714e-07, 1.12730e-07, 1.87727e-07, 2.51509e-01, 2.56099e-01, 3.37083e-01, 3.39137e-01, 3.42637e-01, 3.48691e-01, 3.49059e-01, 3.50360e-01, 3.77199e-01, 3.96925e-01, 3.97760e-01, 4.27904e-01, 4.29469e-01, 4.30729e-01]),
+    (16, [0.00000e+00, 0.00000e+00, 1.39971e-07, 1.98557e-07, 2.46956e-07, 3.04491e-07, 2.59393e-01, 2.60642e-01, 3.51863e-01, 3.52430e-01, 3.53351e-01, 3.57428e-01, 3.57552e-01, 3.57918e-01, 3.99576e-01, 4.05492e-01, 4.05636e-01, 4.52988e-01, 4.53485e-01, 4.54343e-01])
+    )
+    unit_cube_esnice_ssit(d...)
 end
+end # module 
+nothing
