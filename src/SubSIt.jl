d075cac14a8866a760901e4335bbd88fdd45ef33
Petr Krysl <pkrysl@ucsd.edu>
2022-11-20 10:47:00 -0800

    update source

--- src/SubSIt.jl
+++ src/SubSIt.jl
@@ -37,66 +37,108 @@ Keyword arguments
     successive  estimates of the eigenvalues (or not normalized if the 
     eigenvalues converge to zero).
 """
-function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), tol = 1.0e-4, maxiter = 300, verbose=false, which=:SM, check=0) 
+# function ssit(K, M; nev = 6, ncv=max(20, 2*nev+1), v0 = fill(eltype(K), 0, 0), tol = 1.0e-3, maxiter = 300, verbose=false, which=:SM, check=0) 
+#     @assert which == :SM
+#     @assert nev >= 1
+#     ncv = max(ncv, size(v0, 2))
+#     @assert nev < ncv
+#     nvecs = ncv
+#     if size(v0) == (0, 0)
+#         v0 = fill(zero(eltype(K)), size(K,1), nvecs)
+#         for j in axes(M, 1)
+#             v0[j, 1] = M[j, j]
+#         end
+#         dMK = diag(M) ./ diag(K)
+#         ix = sortperm(dMK)
+#         k = 1
+#         for j in 2:nvecs-1
+#             v0[ix[k], j] = 1.0
+#         end
+#         v0[:, end] = rand(size(K,1))
+#     end
+#     factor = cholesky(K)
+#     return _ssit(factor, M, nev, v0, tol, maxiter, verbose) 
+# end
+function ssit(K, M; nev = 6, ncv=0, X = fill(eltype(K), 0, 0), tol = 1.0e-3, maxiter = 300, verbose=false, which=:SM, check=0) 
     @assert which == :SM
     @assert nev >= 1
-    ncv = max(ncv, size(v0, 2))
-    @assert nev < ncv
-    nvecs = ncv
-    if size(v0) == (0, 0)
-        v0 = fill(zero(eltype(K)), size(K,1), nvecs)
-        for j in axes(M, 1)
-            v0[j, 1] = M[j, j]
+
+    factor = cholesky(K)
+
+    _nev = max(Int(round(nev/4)+1), 20)
+    while true
+        ncv = min(_nev + 50, Int(round(_nev * 2.0)))
+        ncv = min(_nev + 50, Int(round(_nev * 2.0)))
+        if size(X) == (0, 0)
+            X = fill(zero(eltype(K)), size(K,1), ncv)
+            for j in axes(M, 1)
+                X[j, 1] = M[j, j]
+            end
+            dMK = diag(M) ./ diag(K)
+            ix = sortperm(dMK)
+            k = 1
+            for j in 2:ncv-1
+                X[ix[k], j] = 1.0
+            end
+            X[:, end] = rand(size(K,1))
+        else
+            X = hcat(X, rand(eltype(K), size(X, 1), ncv - size(X, 2)))
         end
-        dMK = diag(M) ./ diag(K)
-        ix = sortperm(dMK)
-        k = 1
-        for j in 2:nvecs-1
-            v0[ix[k], j] = 1.0
+        _maxiter = ifelse(_nev == nev, maxiter, 4)
+        lamb, X, nconv, niter, lamberr = _ssit(factor, M, _nev, X, tol, _maxiter, verbose) 
+        if _nev == nev
+            return lamb[1:nev], X[:, 1:nev], nconv, niter, lamberr
         end
-        v0[:, end] = rand(size(K,1))
+        _nev = _nev * 2
+        _nev = min(nev, _nev)
     end
-    X = v0
+    return nothing
+end
+
+function _ssit(K, M, nev, X, tol, maxiter, verbose) 
+    nvecs = size(X, 2)
+    @show nev, nvecs
     Y = deepcopy(X)
     Kr = fill(zero(eltype(K)), nvecs, nvecs)
     Mr = fill(zero(eltype(K)), nvecs, nvecs)
+    evalues = fill(zero(eltype(K)), nvecs)
+    evectors = fill(zero(eltype(K)), nvecs, nvecs)
     plamb = fill(zero(eltype(K)), nvecs) .+ Inf
     lamb = fill(zero(eltype(K)), nvecs)
     lamberr = fill(zero(eltype(K)), nvecs)
-    converged = falses(nev)  # not yet
+    converged = falses(nvecs)  # not yet
     niter = 0
     nconv = 0
 
-    factor = cholesky(K)
     mul!(Y, M, X)
     for i = 1:maxiter
         qrd = qr!(Y)
         Y .= Matrix(qrd.Q)
-        X .= factor \ Y
-        mul!(Kr, transpose(X), Y)
+        X .= K \ Y
+        mul!(Kr, Transpose(X), Y)
         mul!(Y, M, X)
-        mul!(Mr, transpose(X), Y)
+        mul!(Mr, Transpose(X), Y)
         decomp = eigen(Kr, Mr)
         ix = sortperm(real.(decomp.values))
-        evalues = real.(decomp.values[ix])
-        evectors = decomp.vectors[:, ix]
-        mul!(X, Y, real.(evectors))
+        evalues .= real.(@view decomp.values[ix])
+        evectors .= real.(@view decomp.vectors[:, ix])
+        mul!(X, Y, evectors)
         # __mass_normalize_M!(X, M)
         X, Y = Y, X
         lamb .= evalues
         for j in 1:nvecs
             lamberr[j] = abs(lamb[j] - plamb[j]) / abs(plamb[j])
         end
-        converged .= (lamberr .<= tol)[1:nev]
+        converged .= (lamberr .<= tol)
         nconv = length(findall(converged))
-        verbose && println("maximum(lamberr)=$(maximum(lamberr[1:nev])), nconv = $(nconv)")
+        verbose && println("maximum(lamberr)=$(maximum(lamberr)), nconv = $(nconv)")
         if nconv >= nev # converged on all requested eigenvalues
             break
         end
         lamb, plamb = plamb, lamb
         niter = niter + 1
     end
-    return lamb[1:nev], Y[:, 1:nev], nconv, niter, lamberr
+    return lamb, Y, nconv, niter, lamberr
 end
 
 function __normalize!(v)
